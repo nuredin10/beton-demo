235ba0488ce01d63e91982cfe71d4946fa9ae248
Alazar Zewdu <alazarzewdu3@gmail.com>
2022-11-19 18:31:10 +0300

    finished removing the sections

--- src/components/section/property-details.js
+++ src/components/section/property-details.js
@@ -120,174 +120,25 @@ class PropertyDetails extends Component {
 			          </div>
 			          
 			              
-			          <div className="single-property-grid">
-			            <h4>Additional Details</h4>
-			            <div className="row">
-			              <div className="col-md-4">
-			                <ul>
-			                  <li>Remodale Year: 3</li>
-			                  <li>Amenites: Half of Fame</li>
-			                  <li>Equepment: Grill-gass</li>
-			                </ul>
-			              </div>
-			              <div className="col-md-4">
-			                <ul>
-			                  <li>Diposit: 7065$</li>
-			                  <li>Pool Size: 1620</li>
-			                  <li>Additional Room: 2</li>
-			                </ul>
-			              </div>
-			              <div className="col-md-4">
-			                <ul>
-			                  <li>Ground: 2</li>
-			                  <li>Additional Room: 2</li>
-			                  <li>Floor: 1203</li>
-			                </ul>
-			              </div>
-			            </div>
-			          </div>
-			        
-			          <div className="single-property-grid">
-			            <h4>Estate Location</h4>
-			            <div className="property-map">
-			              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d198059.49240377638!2d-84.68048827338674!3d39.13652252762691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1615660592820!5m2!1sen!2sbd" />
-			            </div>
-			          </div>
-			          <div className="single-property-grid">
-			            <h4>Floor Plans</h4>
-			            <img src={publicUrl+"assets/img/project-single/6.png"} alt="img" />
-			          </div>
-			          <div className="single-property-grid">
-			            <h4>Intro Video</h4>
-			            <div className="property-video text-center" style={{background: 'url('+publicUrl+'assets/img/project-single/8.png)'}}>
-			              <a className="play-btn" href="https://www.youtube.com/embed/Wimkqo8gDZ0" data-effect="mfp-zoom-in"><i className="fa fa-play" aria-hidden="true" /></a>
-			            </div>
-			          </div>
-			          <div className="single-property-grid">
-			            <h4>Whats Nearby?</h4>
-			            <div className="media single-review-inner">
-			              <div className="media-left">
-			                <div className="thumb">
-			                  <img src={publicUrl+"assets/img/project-single/9.png"} alt="img" />
-			                </div>
-			              </div>
-			              <div className="media-body align-self-center">
-			                <div className="row">
-			                  <div className="col-md-8">
-			                    <h5>Eureka/Harvey Milk Branch</h5>
-			                    <p>consectetuLorem ipsum dolor sit amet</p>
-			                  </div>
-			                  <div className="col-md-4 text-md-right">
-			                    <p className="ratting-title"><span>32</span> Reviews</p>
-			                    <div className="ratting-inner">
-			                      <i className="fa fa-star" />
-			                      <i className="fa fa-star" />
-			                      <i className="fa fa-star" />
-			                      <i className="fa fa-star" />
-			                      <i className="fa fa-star" />
-			                    </div>
-			                  </div>
-			                </div>
-			              </div>
-			            </div>
-			            <div className="media single-review-inner">
-			              <div className="media-left">
-			                <div className="thumb">
-			                  <img src={publicUrl+"assets/img/project-single/10.png"} alt="img" />
-			                </div>
-			              </div>
-			              <div className="media-body align-self-center">
-			                <div className="row">
-			                  <div className="col-md-8">
-			                    <h5>Milbaly Extension &amp; Academy</h5>
-			                    <p>consectetuLorem ipsum dolor sit amet</p>
-			                  </div>
-			                  <div className="col-md-4 text-md-right">
-			                    <p className="ratting-title"><span>32</span> Reviews</p>
-			                    <div className="ratting-inner">
-			                      <i className="fa fa-star" />
-			                      <i className="fa fa-star" />
-			                      <i className="fa fa-star" />
-			                      <i className="fa fa-star" />
-			                      <i className="fa fa-star" />
-			                    </div>
-			                  </div>
-			                </div>
-			              </div>
-			            </div>
-			            <div className="media single-review-inner">
-			              <div className="media-left">
-			                <div className="thumb">
-			                  <img src={publicUrl+"assets/img/project-single/11.png"} alt="img" />
-			                </div>
-			              </div>
-			              <div className="media-body align-self-center">
-			                <div className="row">
-			                  <div className="col-md-8">
-			                    <h5>Nilgao School</h5>
-			                    <p>consectetuLorem ipsum dolor sit amet</p>
-			                  </div>
-			                  <div className="col-md-4 text-md-right">
-			                    <p className="ratting-title"><span>32</span> Reviews</p>
-			                    <div className="ratting-inner">
-			                      <i className="fa fa-star" />
-			                      <i className="fa fa-star" />
-			                      <i className="fa fa-star" />
-			                      <i className="fa fa-star" />
-			                      <i className="fa fa-star" />
-			                    </div>
-			                  </div>
-			                </div>
-			              </div>
-			            </div>
-			          </div>
-			          <div className="single-property-grid">
-			            <h4>Page statistics</h4>
-			            <img src={publicUrl+"assets/img/project-single/7.png"} alt="img" />
-			          </div>
-			          <form className="single-property-comment-form">
-			            <div className="single-property-grid bg-black">
-			              <div className="single-property-form-title">
-			                <div className="row">
-			                  <div className="col-md-8">
-			                    <h4>Post Your Comment</h4>
-			                  </div>
-			                  <div className="col-md-4 text-md-right">
-			                    <div className="ratting-inner">
-			                      <i className="fa fa-star" />
-			                      <i className="fa fa-star" />
-			                      <i className="fa fa-star" />
-			                      <i className="fa fa-star" />
-			                      <i className="fa fa-star" />
-			                    </div>
-			                  </div>
-			                </div>
-			              </div>
-			              <div className="row">
-			                <div className="col-lg-6">
-			                  <label className="single-input-inner style-bg">
-			                    <span className="label">Enter Your Name</span>
-			                    <input type="text" placeholder="Your name here...." />
-			                  </label>
-			                </div>
-			                <div className="col-lg-6">
-			                  <label className="single-input-inner style-bg">
-			                    <span className="label">Enter Your MAil</span>
-			                    <input type="text" placeholder="Your email here...." />
-			                  </label>
-			                </div>
-			                <div className="col-12">
-			                  <label className="single-input-inner style-bg">
-			                    <span className="label">Enter Your Messege</span>
-			                    <textarea placeholder="Enter your messege here...." defaultValue={""} />
-			                  </label>
-			                </div>
-			                <div className="col-12 mb-4">
-			                  <button className="btn btn-base radius-btn">Submit Now</button>
-			                </div>
-			              </div>
-			            </div>
-			          </form>
+			          
+			             
+			              
+			          
+			         
+			              
+			                 
+			                 
+			            
+			                 
+			          
+			         
+			                  
+			              
+			               
+			               
+			                
+			             
+			          
 			        </div>
 			      </div>
 			      <div className="col-lg-4">
@@ -313,58 +164,9 @@ betonproperties@gmail.com</p>
 			            </div>        
 			          </div>
 			          
-			          <div className="widget widget-place">
-			            <h5 className="widget-title">Place</h5>
-			            <ul>
-			              <li>New York <span>26</span></li>
-			              <li>Love Road <span>20</span></li>
-			              <li>Beach Side <span>21</span></li>
-			              <li>Golden city <span>31</span></li>
-			            </ul>
-			          </div>
-			          <div className="widget widget-news">
-			            <h5 className="widget-title">Popular Feeds</h5>
-			            <div className="single-news-wrap media">
-			              <div className="thumb">
-			                <img src={publicUrl+"assets/img/blog/5.png"} alt="img" />
-			              </div>
-			              <div className="media-body align-self-center">
-			                <h6><Link to="/blog-details">Dolor eorem ipsum sit amet Lorem ipsum</Link></h6>
-			                <p className="date"><i className="far fa-calendar-alt" />25 Aug 2020</p>
-			              </div>
-			            </div>
-			            <div className="single-news-wrap media">
-			              <div className="thumb">
-			                <img src={publicUrl+"assets/img/blog/6.png"} alt="img" />
-			              </div>
-			              <div className="media-body align-self-center">
-			                <h6><Link to="/blog-details">Responsive Web And Desktop Develope</Link></h6>
-			                <p className="date"><i className="far fa-calendar-alt" />25 Aug 2020</p>
-			              </div>
-			            </div>
-			            <div className="single-news-wrap media">
-			              <div className="thumb">
-			                <img src={publicUrl+"assets/img/blog/7.png"} alt="img" />
-			              </div>
-			              <div className="media-body align-self-center">
-			                <h6><Link to="/blog-details">Admin Web is Django Highlig Models</Link></h6>
-			                <p className="date"><i className="far fa-calendar-alt" />25 Aug 2020</p>
-			              </div>
-			            </div>
-			          </div>
-			          <div className="widget widget-tags">
-			            <h5 className="widget-title">Popular Tags</h5>
-			            <div className="tagcloud">
-			              <Link to="/blog">Popular</Link>
-			              <Link to="/blog">Art</Link>
-			              <Link to="/blog">Business</Link>
-			              <Link to="/blog">Design</Link>
-			              <Link to="/blog">Creator</Link>
-			              <Link to="/blog">CSS</Link>
-			              <Link to="/blog">Planing</Link>
-			              <Link to="/blog">Creative</Link>
-			            </div>   
-			          </div>
+			          
+			       
+			          
 			        </aside>
 			      </div>
 			    </div>
