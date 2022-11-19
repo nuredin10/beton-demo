a7d641ade02b7c66c3ecd8370155d24eb2d3ff89
Alazar Zewdu <alazarzewdu3@gmail.com>
2022-11-19 18:36:30 +0300

    last update fixed

--- src/components/section/property-details.js
+++ src/components/section/property-details.js
@@ -121,23 +121,25 @@ class PropertyDetails extends Component {
 			          
 			              
 			          
+			        
+			          <div className="single-property-grid">
+			            <h4>Estate Location</h4>
+			            <div className="property-map">
+			              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d198059.49240377638!2d-84.68048827338674!3d39.13652252762691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1615660592820!5m2!1sen!2sbd" />
+			            </div>
+			          </div>
+			         
+			          
 			             
-			              
-			          
-			         
-			              
-			                 
-			                 
-			            
-			                 
-			          
-			         
 			                  
+			           
+			            
 			              
-			               
-			               
-			                
-			             
+			                  
+			           
+			              
+			                  
+			          
 			          
 			        </div>
 			      </div>
